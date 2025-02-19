b5ca32b7c2b712e88be74390e91a8fd9de90da7c
Raid Mowla <dearraid@gmail.com>
2025-02-19 09:47:55 -0600

    Update bedroom.py

--- bedroom.py
+++ bedroom.py
@@ -9,7 +9,6 @@ class Barracks(Room):
 		self.description = """\nYou've entered the barracks. There is large pool of blood by one of the bunks.\nBloodied rags, sheets and other clothing lay on and around the bed.\nA reminder of a failure.\n 
 		You can head west to the reactor, east to the kitchen and south to the dining area.\n"""
 		self.contents = ["Safety Gear"]
-		self.doors = {"w": "reactor", "e": "kitchen", "s":"dining"}
 		self.special = []
 
 	def get_random_enemy(self):
